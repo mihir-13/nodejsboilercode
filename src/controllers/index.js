284ef847c474edcb768159410d8b5b09f464578b
Mihir Brahmbhatt <mihir.brahmbhatt@sayalife.in>
2022-07-15 07:29:00 +0530

    New commit for FORK 2

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -39,3 +39,14 @@ console.log(year + "-" + month + "-" + date + " " + hours + ":" + minutes + ":" 
 
 // prints time in HH:MM format
 console.log(hours + ":" + minutes);
+
+
+let ts = Date.now();
+
+let date_ob = new Date(ts);
+let date = date_ob.getDate();
+let month = date_ob.getMonth() + 1;
+let year = date_ob.getFullYear();
+
+// prints date & time in YYYY-MM-DD format
+console.log(year + "-" + month + "-" + date);
