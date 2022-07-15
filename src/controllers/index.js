882098eb2f050d594bbd1dbfdb53073bf6b63e8a
Mihir Brahmbhatt <mihir.brahmbhatt@sayalife.in>
2022-07-15 07:31:44 +0530

    new feature branch code

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -41,12 +41,39 @@ console.log(year + "-" + month + "-" + date + " " + hours + ":" + minutes + ":" 
 console.log(hours + ":" + minutes);
 
 
-let ts = Date.now();
-
-let date_ob = new Date(ts);
-let date = date_ob.getDate();
-let month = date_ob.getMonth() + 1;
-let year = date_ob.getFullYear();
-
-// prints date & time in YYYY-MM-DD format
-console.log(year + "-" + month + "-" + date);
+var today = new Date();
+  var day = today.getDay();
+  var daylist = ["Sunday","Monday","Tuesday","Wednesday ","Thursday","Friday","Saturday"];
+  console.log("Today is : " + daylist[day] + ".");
+  var hour = today.getHours();
+  var minute = today.getMinutes();
+  var second = today.getSeconds();
+  var prepand = (hour >= 12)? " PM ":" AM ";
+  hour = (hour >= 12)? hour - 12: hour;
+  if (hour===0 && prepand===' PM ') 
+  { 
+  if (minute===0 && second===0)
+  { 
+  hour=12;
+  prepand=' Noon';
+  } 
+  else
+  { 
+  hour=12;
+  prepand=' PM';
+  } 
+  } 
+  if (hour===0 && prepand===' AM ') 
+  { 
+  if (minute===0 && second===0)
+  { 
+  hour=12;
+  prepand=' Midnight';
+  } 
+  else
+  { 
+  hour=12;
+  prepand=' AM';
+  } 
+  } 
+console.log("Current Time : "+hour + prepand + " : " + minute + " : " + second);
