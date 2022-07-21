282a7c7a2d27e39d50592a02216825306d5cfcd3
Mihir Brahmbhatt <mihir.brahmbhatt@sayalife.in>
2022-07-21 07:06:05 +0530

    Commited for Pull request on Thursday

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,7 +1,10 @@
 module.exports.authController = require('./auth.controller');
 module.exports.userController = require('./user.controller');
 
-
 // Need to add new controller
 
 // Or for a pull request by Fork
+
+
+
+/// DO HERE FOR A PULL REQUEST
